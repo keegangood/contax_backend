b465a6a908a7f0fd59f5d4835c7b2ade8c1b3891
keegangood <keegood8@gmail.com>
2021-07-27 14:35:21 -0700

    removed boilerplate and added router

--- contax_frontend/src/App.js
+++ contax_frontend/src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { Router, Route } from "react-router-dom";
+import { createBrowserHistory } from "history";
+
+import "./App.scss";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const history = createBrowserHistory();
+
+  return <div className="App container-fluid">
+    <Router history={history}>
+      <Route exact path="/" component={()=>("Hello")} />
+    </Router>
+  </div>;
 }
 
 export default App;
