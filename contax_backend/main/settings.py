cceedf9ce2feb17defb2be84809314d276c0632a
keegangood <keegood8@gmail.com>
2021-07-27 11:45:11 -0700

    add users

---
+++ contax_backend/main/settings.py
@@ -0,0 +1,172 @@
+from pathlib import Path
+import decouple
+
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = decouple.config('DJANGO_DEBUG')
+
+# set SECRET_KEY based on value of DEBUG
+if DEBUG:
+    SECRET_KEY = decouple.config('DJANGO_SECRET_KEY_DEVELOPMENT')
+else:
+    SECRET_KEY = decouple.config('DJANGO_SECRET_KEY_PRODUCTION')
+
+
+ALLOWED_HOSTS = []
+
+
+# Application definition
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+    'rest_framework',
+    'corsheaders',
+
+    'users',
+
+]
+
+MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'main.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'main.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+
+# Password validation
+# https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/3.2/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.2/howto/static-files/
+
+STATIC_URL = '/static/'
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Secret for encoding User refresh tokens
+REFRESH_TOKEN_SECRET = decouple.config('DJANGO_REFRESH_TOKEN_SECRET')
+
+# Use custom user model for authentication
+AUTH_USER_MODEL = 'users.User'
+
+# define default authentication method in DRF
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'users.authentication.SafeJWTAuthentication'
+    ]
+}
+
+# to accept cookies via axios
+CORS_ALLOW_CREDENTIALS = True
+
+CORS_ORIGIN_WHITELIST = [
+    'http://localhost:3000',
+    'http://localhost:8000',
+    # other whitelisted origins
+]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+    'http://localhost:8000',
+    # other allowed origins...
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    'http://localhost:3000',
+    'http://localhost:8000',
+    # other allowed origins...
+]
+
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'localhost',
+    # other allowed hosts...
+]
+
+CORS_ALLOW_HEADERS = [
+    'authorization',
+    'content-type',
+    'refresh_token'
+    'x-csrftoken',
+    'withcredentials'
+]
---
+++ contax_backend/main/urls.py
@@ -0,0 +1,13 @@
+# main/urls.py
+from django.contrib import admin
+from django.urls import path, include # add
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+
+    # include user app urls
+    path('users/', include('users.urls')), # add
+
+    # add login capability to browsable REST framework api, if desired
+    path('api-auth/', include('rest_framework.urls')), # add (optional)
+]
