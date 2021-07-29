b195d473738f2f51133df1a4bd13269385002f7d
keegangood <keegood8@gmail.com>
2021-07-29 14:22:35 -0700

    request accessToken and getContacts on success

--- contax_frontend/src/pages/Contacts/Contacts.js
+++ contax_frontend/src/pages/Contacts/Contacts.js
@@ -1,24 +1,39 @@
-import React, {useEffect} from 'react'
+import React, { useEffect } from "react";
 import { useDispatch, useSelector, connect } from "react-redux";
 
-import {Container} from 'reactstrap';
+import { Container } from "reactstrap";
+
+import { requestAccessToken } from "../../state/AuthSlice";
+import { getContacts, setContacts } from "../../state/ContactSlice";
 
 const Contacts = () => {
+  const dispatch = useDispatch();
 
-  useEffect(()=>{
-    const fetchContacts = async () => {}
-  },[])
+  const {user} = useSelector(state=>state.auth)
 
-  return (
-    <Container>
-      Contacts
-    </Container>
-  )
-}
+  useEffect(() => {
+    const fetchContacts = async () => {
+      await dispatch(requestAccessToken())
+        .then((res) => {
+          // status will either be 'fulfilled' or 'rejected'
+          const status = res.meta.requestStatus;
 
-const mapStateToProps = state => {
-  return {
-  }
-}
+          if(status === 'fulfilled'){
+            dispatch(getContacts(user))
+          } else if(status === 'rejected'){
+            console.log('failure', res.payload);
+          }
+        })
+    };
 
-export default connect(mapStateToProps)(Contacts)
+    fetchContacts();
+  }, []);
+
+  return <Container>Contacts</Container>;
+};
+
+const mapStateToProps = (state) => {
+  return {};
+};
+
+export default connect(mapStateToProps)(Contacts);
