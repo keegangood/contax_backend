272f8ad4d718853927f2c917daca8a5558e4d97f
keegangood <keegood8@gmail.com>
2021-07-29 14:15:33 -0700

    included contact urls

--- contax_backend/main/urls.py
+++ contax_backend/main/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     # include user app urls
     path('users/', include('users.urls')), # add
 
+    path('contacts/', include('contacts.urls')),
     # add login capability to browsable REST framework api, if desired
     # path('api-auth/', include('rest_framework.urls')), # add (optional)
 ]
