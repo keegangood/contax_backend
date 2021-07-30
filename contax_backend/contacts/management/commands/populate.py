8b99743c48d423d7979f78c3e48d24ad4c7c28fb
unknown <keegood8@gmail.com>
2021-07-30 14:27:09 -0700

    added chance of None for phone_number. added birthday and age

--- contax_backend/contacts/management/commands/populate.py
+++ contax_backend/contacts/management/commands/populate.py
@@ -1,5 +1,7 @@
 import requests
 import random
+from datetime import date
+from faker import Faker
 from django.core.management.base import BaseCommand
 from django.contrib.auth import get_user_model
 
@@ -7,21 +9,28 @@ from contacts.models import Contact
 from contacts.serializers import ContactCreateSerializer
 
 def generate_phone_number():
-    chance = random.random()
+    chance_of_existing = random.random()
 
-    digits = 10
-    if chance > .7:
-        digits = 15
-    return ''.join([str(random.randint(0,9)) for _ in range(digits)])
+    if chance_of_existing < .3:
+        phone_number = None
+    else:
+        digits = 10
 
+        # digit_chance = random.random()
+        # if chance > .7:
+            # for international numbers
+            #     digits = 15
+
+        phone_number = ''.join([str(random.randint(0,9)) for _ in range(digits)])
+
+    return phone_number
 
 class Command(BaseCommand):
     help = 'Populates 10 Contacts.'
 
     def handle(self, *args, **options):
+        fake = Faker()
         user = get_user_model().objects.all().first()
-
-        print(user)
 
         users = requests.get("https://jsonplaceholder.typicode.com/users").json()
 
@@ -34,12 +43,22 @@ class Command(BaseCommand):
             work_phone_number = generate_phone_number()
             
             email = user_data['email']
+
+            birthday = fake.date_between(start_date=date(1940, 1, 1), end_date=date(2000, 1, 1))
             
+            today = date.today()
+            age = today.year - birthday.year
+
+            if today.month < birthday.month:
+                age -= 1
+
             contact_serializer = ContactCreateSerializer(data={
                 "user":user.id,
                 "first_name":first_name,
                 "last_name":last_name,
                 "email":email,
+                "birthday": birthday,
+                "age": age,
                 "home_phone_number":home_phone_number,
                 "cell_phone_number":cell_phone_number,
                 "work_phone_number":work_phone_number,
