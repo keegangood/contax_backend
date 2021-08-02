1c22a5e123fa354e395436cb0e0384d9508d1d74
unknown <keegood8@gmail.com>
2021-08-02 10:23:51 -0700

    changed div to reactstrap Row

--- contax_frontend/src/pages/Contacts/ContactList.js
+++ contax_frontend/src/pages/Contacts/ContactList.js
@@ -1,16 +1,18 @@
 import React from "react";
 
+import {Row} from 'reactstrap';
+
 import ContactItem from "./ContactItem";
 
 import './scss/ContactList.scss';
 
-const ContactList = ({ contacts }) => {
+const ContactList = ({ contacts, history }) => {
   return (
-    <div className="row mx-3 py-3" id="contact-list">
+    <Row className="g-0 mx-3 py-3" id="contact-list">
       {contacts.map((contact, i) => (
           <ContactItem contact={contact} key={i} />
       ))}
-    </div>
+    </Row>
   );
 };
 
