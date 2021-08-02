819551793c0b582dec7176bcfd8e199646a74e41
unknown <keegood8@gmail.com>
2021-08-02 10:20:52 -0700

    setup added ContactForm

--- contax_frontend/src/pages/Contacts/Contacts.js
+++ contax_frontend/src/pages/Contacts/Contacts.js
@@ -1,34 +1,41 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector, connect } from "react-redux";
+import { Switch, Route, useRouteMatch, useParams } from "react-router-dom";
 
-import { Container } from "reactstrap";
+import { Container, Row, Col } from "reactstrap";
 
+import PlusButton from "../../components/PlusButton";
 import ContactList from "./ContactList";
+import ContactForm from "./ContactForm";
 
 import { requestAccessToken } from "../../state/AuthSlice";
-import { getContacts, setContacts } from "../../state/ContactSlice";
+import {
+  getContacts,
+  setContacts,
+  createContact,
+} from "../../state/ContactSlice";
 
-import './scss/Contacts.scss';
+import "./scss/Contacts.scss";
 
-const Contacts = () => {
+const Contacts = ({ history }) => {
+  const { url, path } = useRouteMatch();
+  const { formAction } = useParams();
+  console.log(url, path);
   const dispatch = useDispatch();
 
   const { user } = useSelector((state) => state.auth);
-  const { contacts } = useSelector((state) => state.contacts);
+  const { contacts, orderBy } = useSelector((state) => state.contacts);
 
   useEffect(() => {
     const fetchContacts = async () => {
       await dispatch(requestAccessToken())
         .then((res) => {
           // requestStatus will either be 'fulfilled' or 'rejected'
-
-          console.log(res);
-
           const { requestStatus } = res.meta;
           const { accessToken } = res.payload;
 
           if (requestStatus === "fulfilled") {
-            dispatch(getContacts(accessToken));
+            dispatch(getContacts({ accessToken, orderBy }));
           } else if (requestStatus === "rejected") {
             console.log("failure", res.payload);
           }
@@ -39,13 +46,31 @@ const Contacts = () => {
     fetchContacts();
   }, []);
 
+  const addContact = (formData) => {
+    dispatch(createContact(formData));
+  };
+
+  console.log(path, url);
+
   return (
     <Container>
-      <div className="row g-0">
-        <div className="col col-12 col-md-8 offset-md-2">
-          <ContactList contacts={contacts} />
-        </div>
-      </div>
+      <Row className="g-0">
+        <Col sm={12} md={{ size: 10, offset: 1 }}>
+          {formAction === "add" ? (
+            <ContactForm callApi={addContact} />
+          ) : formAction === "edit" ? (
+            <ContactForm callApi={addContact} />
+          ) : (
+            ""
+          )}
+          {!formAction && (
+            <>
+              <ContactList contacts={contacts} />
+              <PlusButton history={history} />
+            </>
+          )}
+        </Col>
+      </Row>
     </Container>
   );
 };
