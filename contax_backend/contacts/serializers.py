cfb04a1d4c3daff3f1a5db561078eb10672c942c
unknown <keegood8@gmail.com>
2021-08-09 12:34:09 -0700

    added additional fields to CreateSerializer

--- contax_backend/contacts/serializers.py
+++ contax_backend/contacts/serializers.py
@@ -5,7 +5,16 @@ from .models import Contact
 class ContactCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Contact
-        fields=['first_name', 'last_name']
+        fields=[
+            'first_name',
+            'last_name',
+            'email',
+            'home_phone_number',
+            'cell_phone_number',
+            'work_phone_number',
+            'birthday',
+            'notes'
+        ]
 
 class ContactDetailSerializer(serializers.ModelSerializer):
     class Meta:
