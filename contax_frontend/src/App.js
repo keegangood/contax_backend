c65eedc8e9da64ba83b20e459aecef0401feab38
unknown <keegood8@gmail.com>
2021-08-09 12:36:35 -0700

    moved store from App.js to index.js

--- contax_frontend/src/App.js
+++ contax_frontend/src/App.js
@@ -1,63 +1,94 @@
+import { useEffect } from "react";
 import { Router, Route, Switch } from "react-router-dom";
 import { createBrowserHistory } from "history";
-import { Provider } from "react-redux";
+import { useDispatch, useSelector, connect } from "react-redux";
 
 import "./App.scss";
-
-import { configureStore, applyMiddleware } from "@reduxjs/toolkit";
-import rootReducer from "./state";
-import thunk from "redux-thunk";
 
 import { Container } from "reactstrap";
 import Homepage from "./pages/Homepage/Homepage";
 import UserAuth from "./pages/UserAuth/UserAuth";
 import Contacts from "./pages/Contacts/Contacts";
 
-let store = configureStore({ reducer: rootReducer }, applyMiddleware(thunk));
+import PrivateRoute from "./components/PrivateRoute";
+
+import { requestAccessToken, getUser } from "./state/AuthSlice";
 
 function App() {
   let history = createBrowserHistory();
+  const dispatch = useDispatch();
+  let { isAuthenticated, authLoadingStatus, user } = useSelector(
+    (state) => state.auth
+  );
+  let {contactLoadingStatus}=useSelector(state=>state.contacts)
+
+  useEffect(() => {
+    if (!isAuthenticated && !user) {
+      (async () => {
+        console.log("app loaded");
+        await dispatch(requestAccessToken());
+      })();
+    }
+  }, [isAuthenticated, user]);
 
   return (
-    <Provider store={store}>
-      <Container fluid className="g-0">
-        <Router history={history}>
-          <Switch>
-            <Route exact path="/" component={Homepage} />
-            <Route path="/app/:formAction?" history={history}>
-              <Contacts />
-            </Route>
-            <Route
-              exact
-              path="/login"
-              render={(props) => {
-                return (
-                  <UserAuth
-                    pageAction={"login"}
-                    pageTitle={"Log in"}
-                    history={history}
-                  />
-                );
-              }}
-            />
-            <Route
-              exact
-              path="/signup"
-              render={(props) => {
-                return (
-                  <UserAuth
-                    pageAction={"signup"}
-                    pageTitle={"Sign up"}
-                    history={history}
-                  />
-                );
-              }}
-            />
-          </Switch>
-        </Router>
-      </Container>
-    </Provider>
+    <Container fluid className="g-0">
+      <Router history={history}>
+        <Switch>
+          <Route exact path="/" component={Homepage} />
+          <PrivateRoute
+            exact
+            path="/app"
+            history={history}
+            isAuthenticated={isAuthenticated}
+            loadingStatuses={[authLoadingStatus, contactLoadingStatus]}
+            component={Contacts}
+          />
+          <PrivateRoute
+          exact
+            path="/app/:formAction?/:contactId?"
+            history={history}
+            isAuthenticated={isAuthenticated}
+            authLoadingStatus={authLoadingStatus}
+            component={Contacts}
+          />
+          <Route
+            exact
+            path="/login"
+            render={(props) => {
+              return (
+                <UserAuth
+                  pageAction={"login"}
+                  pageTitle={"Log in"}
+                  history={history}
+                />
+              );
+            }}
+          />
+          <Route
+            exact
+            path="/signup"
+            render={(props) => {
+              return (
+                <UserAuth
+                  pageAction={"signup"}
+                  pageTitle={"Sign up"}
+                  history={history}
+                />
+              );
+            }}
+          />
+        </Switch>
+      </Router>
+    </Container>
   );
 }
 
-export default App;
+const mapPropsToState = (state) => {
+  return {
+    isAuthenticated: state.auth.isAuthenticated,
+    authLoadingStatus: state.auth.authLoadingStatus,
+  };
+};
+
+export default connect(mapPropsToState)(App);
