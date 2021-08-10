406a63faac0b5863ab3934f12ba142b9d49e5a66
unknown <keegood8@gmail.com>
2021-08-10 10:28:52 -0700

    added referrer path for redirect after login

--- contax_frontend/src/components/PrivateRoute.js
+++ contax_frontend/src/components/PrivateRoute.js
@@ -5,24 +5,27 @@ const PrivateRoute = ({
   component: Component,
   isAuthenticated,
   authLoadingStatus,
+  history,
   ...rest
 }) => {
   return (
-      <Route
-        // pass the rest of the props
-        {...rest}
-        render={(props) =>
-          // if not authenticated when loaded, redirect to login page
-          !isAuthenticated && authLoadingStatus === "IDLE" ? (
-            <Redirect to={{
-              pathname:'/login',
-              
-            }}/>
-            ) : (
-            <Component {...props} />
-          )
-        }
-      />
+    <Route
+      // pass the rest of the props
+      {...rest}
+      render={(props) =>
+        // if not authenticated when loaded, redirect to login page
+        !isAuthenticated && authLoadingStatus === "IDLE" ? (
+          <Redirect
+            to={{
+              pathname: "/login",
+              state: { referrer: props.history.location.pathname },
+            }}
+          />
+        ) : (
+          <Component {...props} />
+        )
+      }
+    />
   );
 };
 
