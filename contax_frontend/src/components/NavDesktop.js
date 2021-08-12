0bd9de3608f701062ae28dc547e7700ab64aeba6
unknown <keegood8@gmail.com>
2021-08-11 17:21:19 -0700

    reset bootstrap classes on nav-links

--- contax_frontend/src/components/NavDesktop.js
+++ contax_frontend/src/components/NavDesktop.js
@@ -1,39 +1,38 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import {
-  Navbar,
-  Nav,
-  NavLink,
-} from "reactstrap";
+import { Navbar, Nav, NavLink } from "reactstrap";
 
 import Avatar from "./Avatar";
 
 import "./scss/NavDesktop.scss";
 
-
 const NavDesktop = ({ user }) => {
   return (
-    <Navbar id="nav-desktop" className="position-absolute ps-3 shadow">
+    <Navbar id="nav-desktop" className="position-absolute ps-3 shadow border-bottom border-3 border-info">
       <Link to="/app" id="navbar-title" className="p-0">
         Contax
       </Link>
       <Nav className="ml-auto">
-        <NavLink tag={Link} to="/about" className="py-0">
+        <NavLink tag={Link} to="/app">
+          Home
+        </NavLink>
+        <NavLink tag={Link} to="/about">
           About
         </NavLink>
+        {user && <NavLink tag={Link}>Log out</NavLink>}
         <span className="d-flex">
           {user ? (
             <>
-              <NavLink tag={Link} to="/login" className="py-0">
+              <NavLink tag={Link} to="/login" className="py-0 my-auto">
                 <Avatar contact={user} />
               </NavLink>
             </>
           ) : (
             <>
-              <NavLink tag={Link} to="/login" className="py-0">
+              <NavLink tag={Link} to="/login">
                 Log in
               </NavLink>
-              <NavLink tag={Link} to="/signup" className="py-0">
+              <NavLink tag={Link} to="/signup">
                 Sign up
               </NavLink>
             </>
