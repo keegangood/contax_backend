e96a343907092d4dbb1cc91a0852c48f9170850d
unknown <keegood8@gmail.com>
2021-08-11 17:22:18 -0700

    adjusted style and conditional rendering

--- contax_frontend/src/components/NavMobile.js
+++ contax_frontend/src/components/NavMobile.js
@@ -1,54 +1,116 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { NavLink, Row, Col } from "reactstrap";
-import { AiOutlineMenu } from "react-icons/ai";
+import { NavLink, Row, Col, Fade } from "reactstrap";
 import Avatar from "./Avatar";
 
 import "./scss/NavMobile.scss";
 
-const NavMobile = ({ user }) => {
-  return (
-    <div id="nav-mobile" className="position-absolute position-fixed top-0">
-      <span id="hamburger-icon" className="position-absolute end-0 p-2">
-        <AiOutlineMenu />
-      </span>
+const NavMobile = ({ user, onLogout, navOpen, setNavOpen }) => {
+  const [isHidden, setIsHidden] = useState(true);
 
-      <Row id="nav-content" className="g-0">
-        <Col xs={12}>
-          <Link to="/app" id="navbar-title" className="px-3">
-            Contax
-          </Link>
-        </Col>
-        <Col xs={12} className="d-flex align-items-center py-3 my-3">
-          {user ? (
-            <>
-              <NavLink tag={Link} to="/login" className="py-0">
-                <Avatar contact={user} />
-              </NavLink>
-              <span id="username-label">Logged in as:</span>
-              <span id="username-value" className="ps-2">{user.username}</span>
-            </>
-          ) : (
-            <>
-              <NavLink tag={Link} to="/login" className="py-0">
-                Log in
-              </NavLink>
-              <NavLink tag={Link} to="/signup" className="py-0">
-                Sign up
-              </NavLink>
-            </>
-          )}
-        </Col>
-        <Col xs={12} className="py-3 my-3">
-          <NavLink tag={Link} to="/about">
-            Home
-          </NavLink>
-          <NavLink tag={Link} to="/about">
-            About
-          </NavLink>
-        </Col>
-      </Row>
-    </div>
+  return (
+    <>
+      {navOpen && (
+        <Fade
+          in={navOpen}
+          id="nav-mobile"
+          className="position-absolute position-fixed top-0"
+        >
+          <Fade in={navOpen} id="nav-content">
+            <Row className="g-0 pb-5 border-bottom border-info border-5">
+              <Col xs={12} className="mb-3">
+                <Link
+                  to="/app"
+                  id="navbar-title"
+                  className="px-3"
+                  onClick={() => {
+                    setNavOpen(!navOpen);
+                  }}
+                >
+                  Contax
+                </Link>
+              </Col>
+              <Col
+                xs={6}
+                className="
+                d-flex
+                flex-column
+                align-items-center
+                justify-content-center
+                pb-5
+              "
+              >
+                {user ? (
+                  <>
+                    <Avatar
+                      contact={user}
+                      border={{ color: "secondary", width: 3 }}
+                    />
+                    <div id="username-value" className="">
+                      {user.username}
+                    </div>
+                  </>
+                ) : (
+                  <div className="align-self-end mb-auto">
+                    <NavLink
+                      className="text-right"
+                      tag={Link}
+                      to="/login"
+                      onClick={() => {
+                        setNavOpen(!navOpen);
+                      }}
+                    >
+                      Log in
+                    </NavLink>
+                    <NavLink
+                      className="text-right"
+                      tag={Link}
+                      to="/signup"
+                      onClick={() => {
+                        setNavOpen(!navOpen);
+                      }}
+                    >
+                      Sign up
+                    </NavLink>
+                  </div>
+                )}
+              </Col>
+              <Col xs={6} className="pb-5">
+                <NavLink
+                  tag={Link}
+                  to="/app"
+                  onClick={() => {
+                    setNavOpen(!navOpen);
+                  }}
+                >
+                  Home
+                </NavLink>
+                <NavLink
+                  tag={Link}
+                  to="/about"
+                  onClick={() => {
+                    setNavOpen(!navOpen);
+                  }}
+                >
+                  About
+                </NavLink>
+                {user && (
+                  <NavLink
+                    tag={Link}
+                    onClick={() => {
+                      setNavOpen(!navOpen);
+                      onLogout();
+                    }}
+                  >
+                    Log out
+                  </NavLink>
+                )}
+              </Col>
+            </Row>
+          </Fade>
+        </Fade>
+      )}
+    </>
   );
 };
 
