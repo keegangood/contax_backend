11f8e149dddc93c565c19129a9d23b47db7d7f1a
unknown <keegood8@gmail.com>
2021-08-11 17:20:20 -0700

    added optional border

--- contax_frontend/src/components/Avatar.js
+++ contax_frontend/src/components/Avatar.js
@@ -1,17 +1,18 @@
 import React from "react";
 import { AiOutlineCamera } from "react-icons/ai";
 
-const Avatar = ({ contact }) => {
+const Avatar = (props) => {
+  console.log(props);
+
   return (
     <span
-      className="
-            contact-avatar 
-            rounded-circle
-            bg-success 
-            d-flex
-            align-items-center 
-            justify-content-center 
-            text-secondary"
+      className={
+        "contact-avatar rounded-circle bg-success " +
+        "d-flex align-items-center justify-content-center text-secondary" +
+        (props.border
+          ? `border border-${props.border.color} border-${props.border.width}`
+          : "")
+      }
     >
       {/* LOAD USER AVATAR ONCE AVAILABLE */}
       <AiOutlineCamera />
