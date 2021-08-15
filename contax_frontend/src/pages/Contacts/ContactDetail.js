816977b20ea3bcd030715100ace4142d8b0aff56
unknown <keegood8@gmail.com>
2021-08-15 13:00:31 -0700

    added conditional rendering while contact is loading

--- contax_frontend/src/pages/Contacts/ContactDetail.js
+++ contax_frontend/src/pages/Contacts/ContactDetail.js
@@ -1,7 +1,7 @@
 import React, { Fragment, useState, useEffect } from "react";
 import dayjs from "dayjs";
 import { useDispatch, useSelector, connect } from "react-redux";
-import { Link, useParams } from "react-router-dom";
+import { Link, useParams, useRouteMatch } from "react-router-dom";
 import { unwrapResult } from "@reduxjs/toolkit";
 
 import titleize from "../../utils/titleize";
@@ -33,6 +33,7 @@ const ContactDetail = ({ history }) => {
   const PHONE_TYPES = ["CELL", "HOME", "WORK"];
   const dispatch = useDispatch();
   const { contactId } = useParams();
+  const { path } = useRouteMatch();
   const { currentContact, contactLoadingStatus } = useSelector(
     (state) => state.contacts
   );
@@ -90,77 +91,87 @@ const ContactDetail = ({ history }) => {
           <Spinner color="info"> </Spinner>
         </div>
       ) : (
-        <Container className="py-5" id="contact-detail-content">
-          <Row className="g-0 mt-5 pt-5">
-            <Col
-              xs={12}
-              md={{ size: 10, offset: 1 }}
-              className="
-                bg-secondary
-                rounded
-              "
-            >
-              {/* DETAIL HEADER */}
-              <Row
+        currentContact && (
+          <Container className="py-5" id="contact-detail-content">
+            <Row className="g-0 mt-5 pt-5">
+              <Col
+                xs={12}
+                md={{ size: 10, offset: 1 }}
                 className="
-                  g-0
-                  pb-3
-                  border-bottom
-                  border-3
-                  border-info
-                "
+              bg-secondary
+              rounded
+              "
               >
-                <Col
-                  xs={{ size: 6, offset: 3 }}
-                  className="d-flex justify-content-center p-3"
-                >
-                  <Avatar className="contact-avatar" />
-                </Col>
-                <Col
-                  xs={3}
+                {/* DETAIL HEADER */}
+                <Row
                   className="
-                    close-icon
-                    d-flex justify-content-end
-                    p-3
+                g-0
+                pb-3
+                border-bottom
+                border-3
+                border-info
+                "
+                >
+                  <Col
+                    xs={{ size: 6, offset: 3 }}
+                    className="d-flex justify-content-center p-3"
+                  >
+                    <Avatar className="contact-avatar" />
+                  </Col>
+                  <Col
+                    xs={3}
+                    className="
+                  close-icon
+                  d-flex justify-content-end
+                  p-3
                   "
-                >
-                  <AiOutlineCloseCircle />
-                </Col>
-                <Col xs={12} className="contact-full-name text-center">
-                  {currentContact.firstName} {currentContact.lastName}
-                </Col>
-                <Col
-                  xs={12}
-                  className="d-flex align-items-center justify-content-end mt-3"
-                >
-                  <Link to={`/app/edit/${contact.id}`}>
-                    <AiOutlineEdit
-                      className="crud-icon edit-icon m-2"
-                      onClick={() => {
-                        dispatch(setCurrentContact(contact));
+                  >
+                    <AiOutlineCloseCircle />
+                  </Col>
+                  <Col xs={12} className="contact-full-name text-center">
+                    {firstName} {lastName}
+                  </Col>
+                  <Col
+                    xs={{ size: 6, offset: 3 }}
+                    className="
+                    d-flex
+                    align-items-center justify-content-center 
+                    justify-content-md-end
+                    mt-3"
+                  >
+                    <Link
+                      to={{
+                        pathname: `/app/edit/${contact.id}`,
+                        state: { referer: path },
                       }}
-                    />
-                  </Link>
-                  <span className="position-relative">
-                    <AiOutlineDelete
-                      id={`contact-${contact.id}-popover`}
-                      className="crud-icon delete-icon m-2"
-                      // onClick={() => togglePopover(contact.id, !popoverIsOpen)}
-                    />
-                  </span>
-                </Col>
-              </Row>
+                    >
+                      <AiOutlineEdit
+                        className="crud-icon edit-icon m-2"
+                        onClick={() => {
+                          dispatch(setCurrentContact(contact));
+                        }}
+                      />
+                    </Link>
+                    <span className="position-relative">
+                      <AiOutlineDelete
+                        id={`contact-${contact.id}-popover`}
+                        className="crud-icon delete-icon m-2"
+                        // onClick={() => togglePopover(contact.id, !popoverIsOpen)}
+                      />
+                    </span>
+                  </Col>
+                </Row>
 
-              {/* DETAIL FIELDS */}
-              <Row className="g-0 p-3 bg-primary">
-                {/* EMAIL FIELD */}
-                <Col
-                  xs={2}
-                  md={1}
-                  className="
-                    field-icon
-                    border-bottom
-                    border-secondary
+                {/* DETAIL FIELDS */}
+                <Row className="g-0 p-3 bg-primary">
+                  {/* EMAIL FIELD */}
+                  <Col
+                    xs={2}
+                    md={1}
+                    className="
+                  field-icon
+                  border-bottom
+                  border-secondary
                     pt-3
                     pb-2
                     mb-3
@@ -169,177 +180,180 @@ const ContactDetail = ({ history }) => {
                     justify-content-center
                     align-items-center
                   "
-                >
-                  <AiOutlineMail className="text-secondary" />
-                </Col>
-                <Col
-                  xs={10}
-                  md={11}
-                  className="
-                    contact-field
-                    border-bottom
-                    border-secondary
-                    mb-3 mb-md-4
-                    ps-3
-                    pb-2
-                    d-flex align-items-end
-                  "
-                >
-                  {currentContact.email ? currentContact.email : "Not Provided"}
-                </Col>
-                {/* PHONE FIELDS */}
-                <Col
-                  xs={2}
-                  md={1}
-                  className="
-                    field-icon
-                    border-bottom
-                    border-secondary
-                    pt-md-2
-                    mb-3
-                    mb-md-4
-                    d-flex
-                    justify-content-center
-                  "
-                >
-                  <AiOutlinePhone className="text-secondary" />
-                </Col>
-                <Col
-                  xs={10}
-                  md={5}
-                  className="
-                    contact-field
-                    border-bottom
-                    border-secondary
-                    mb-3 mb-md-4
-                    ps-3 pb-3
-                  "
-                >
-                  {PHONE_TYPES.map((PHONE_TYPE) => (
-                    <Row className="g-0">
-                      <Col
-                        xs={2}
-                        className="
-                          phone-type-label
-                          d-flex align-items-center
-                          small pt-2 mb-2
-                          border-bottom border-secondary
-                        "
-                      >
-                        {titleize(PHONE_TYPE)}
-                      </Col>
-                      <Col
-                        xs={6}
-                        sm={7}
-                        className="border-bottom border-secondary mb-2 pt-2 ps-2"
-                      >
-                        {formatPhoneNumber(
-                          currentContact[
-                            `${PHONE_TYPE.toLowerCase()}PhoneNumber`
-                          ] || ""
-                        )}
-                      </Col>
-                      <Col
-                        xs={2}
-                        md={2}
-                        className="
-                          text-warning ps-3 
-                          border-bottom
-                          border-secondary
-                          mb-2
-                          pt-2 pt-md-0
-                        "
-                      >
-                        {currentContact[
-                          `${currentContact.primaryPhone.toLowerCase()}PhoneNumber`
-                        ] &&
-                          currentContact.primaryPhone === PHONE_TYPE && (
-                            <AiFillStar title="Primary phone" />
+                  >
+                    <AiOutlineMail className="text-secondary" />
+                  </Col>
+                  <Col
+                    xs={10}
+                    md={11}
+                    className="
+                      contact-field
+                      border-bottom
+                      border-secondary
+                      mb-3 mb-md-4
+                      ps-3
+                      pb-2
+                      d-flex align-items-end
+                    "
+                  >
+                    {currentContact.email
+                      ? currentContact.email
+                      : "Not Provided"}
+                  </Col>
+                  {/* PHONE FIELDS */}
+                  <Col
+                    xs={2}
+                    md={1}
+                    className="
+                      field-icon
+                      border-bottom
+                      border-secondary
+                      pt-md-2
+                      mb-3
+                      mb-md-4
+                      d-flex
+                      justify-content-center
+                    "
+                  >
+                    <AiOutlinePhone className="text-secondary" />
+                  </Col>
+                  <Col
+                    xs={10}
+                    md={5}
+                    className="
+                      contact-field
+                      border-bottom
+                      border-secondary
+                      mb-3 mb-md-4
+                      ps-3 pb-3
+                    "
+                  >
+                    {PHONE_TYPES.map((PHONE_TYPE) => (
+                      <Row className="g-0">
+                        <Col
+                          xs={2}
+                          className="
+                            phone-type-label
+                            d-flex align-items-center
+                            small pt-2 mb-2
+                            border-bottom border-secondary
+                          "
+                        >
+                          {titleize(PHONE_TYPE)}
+                        </Col>
+                        <Col
+                          xs={6}
+                          sm={7}
+                          className="border-bottom border-secondary mb-2 pt-2 ps-2"
+                        >
+                          {formatPhoneNumber(
+                            currentContact[
+                              `${PHONE_TYPE.toLowerCase()}PhoneNumber`
+                            ] || ""
                           )}
-                      </Col>
-                    </Row>
-                  ))}
-                </Col>
+                        </Col>
+                        <Col
+                          xs={2}
+                          md={2}
+                          className="
+                            text-warning ps-3 
+                            border-bottom
+                            border-secondary
+                            mb-2
+                            pt-2 pt-md-0
+                          "
+                        >
+                          {currentContact[
+                            `${currentContact.primaryPhone.toLowerCase()}PhoneNumber`
+                          ] &&
+                            currentContact.primaryPhone === PHONE_TYPE && (
+                              <AiFillStar title="Primary phone" />
+                            )}
+                        </Col>
+                      </Row>
+                    ))}
+                  </Col>
 
-                {/* BIRTHDAY FIELD */}
-                <Col
-                  xs={2}
-                  md={1}
-                  className="
-                    field-icon
-                    border-bottom
-                    border-secondary
-                    pt-3 pt-md-2
-                    pb-2 
-                    mb-3
-                    mb-md-4
-                    d-flex
-                    justify-content-center
-                    align-items-center
-                    justify-content-md-start
-                    align-items-md-start
-                  "
-                >
-                  <RiCake2Line className="text-secondary" />
-                </Col>
-                <Col
-                  xs={10}
-                  md={5}
-                  className="
-                    contact-field
-                    border-bottom
-                    border-secondary
-                    mb-3 mb-md-4
-                    pb-2 pt-md-2
-                    ps-3 ps-lg-0
-                    d-flex align-items-end
-                    align-items-md-start
-                  "
-                >
-                  {currentContact.birthday
-                    ? dayjs(currentContact.birthday).format("M / DD / YYYY")
-                    : "Not Provided"}
-                </Col>
+                  {/* BIRTHDAY FIELD */}
+                  <Col
+                    xs={2}
+                    md={1}
+                    className="
+                      field-icon
+                      border-bottom
+                      border-secondary
+                      pt-3 pt-md-2
+                      pb-2 
+                      mb-3
+                      mb-md-4
+                      d-flex
+                      justify-content-center
+                      align-items-center
+                      justify-content-md-start
+                      align-items-md-start
+                    "
+                  >
+                    <RiCake2Line className="text-secondary" />
+                  </Col>
+                  <Col
+                    xs={10}
+                    md={5}
+                    className="
+                      contact-field
+                      border-bottom
+                      border-secondary
+                      mb-3 mb-md-4
+                      pb-2 pt-md-2
+                      ps-3 ps-lg-0
+                      d-flex align-items-end
+                      align-items-md-start
+                    "
+                  >
+                    {currentContact.birthday
+                      ? dayjs(currentContact.birthday).format("M / DD / YYYY")
+                      : "Not Provided"}
+                  </Col>
 
-                {/* NOTES FIELD */}
-                <Col
-                  xs={2}
-                  md={1}
-                  className="
-                    field-icon
-                    border-bottom
-                    border-secondary
-                    pt-3 pt-lg-0
-                    mb-3
-                    pb-2
-                    mb-md-4
-                    d-flex
-                    justify-content-center
-                  "
-                >
-                  <AiOutlineFileText className="text-secondary" />
-                </Col>
-                <Col
-                  xs={10}
-                  md={11}
-                  className="
-                    contact-field
-                    border-bottom
-                    border-secondary
-                    mb-3 mb-md-4
-                    py-3 py-lg-0
-                    ps-3
-                    d-flex flex-column
-                  "
-                >
-                  {currentContact.notes.map((note, i) => (
-                    <span className="pb-2">&bull; {note.text}</span>
-                  ))}
-                </Col>
-              </Row>
-            </Col>
-          </Row>
-        </Container>
+                  {/* NOTES FIELD */}
+                  <Col
+                    xs={2}
+                    md={1}
+                    className="
+                      field-icon
+                      border-bottom
+                      border-secondary
+                      pt-3 pt-lg-0
+                      mb-3
+                      pb-2
+                      mb-md-4
+                      d-flex
+                      justify-content-center
+                    "
+                  >
+                    <AiOutlineFileText className="text-secondary" />
+                  </Col>
+                  <Col
+                    xs={10}
+                    md={11}
+                    className="
+                      contact-field
+                      border-bottom
+                      border-secondary
+                      mb-3 mb-md-4
+                      py-3 py-lg-0
+                      ps-3
+                      d-flex flex-column
+                    "
+                  >
+                    {currentContact.notes.map((note, i) => (
+                      <span className="pb-2">&bull; {note.text}</span>
+                    ))}
+                  </Col>
+                </Row>
+              </Col>
+            </Row>
+          </Container>
+        )
       )}
     </>
   );
