7c784feebc1c6ea028bcf39ed8da6d727a044a45
unknown <keegood8@gmail.com>
2021-08-16 10:38:16 -0700

    set currentContact to null

--- contax_frontend/src/components/PlusButton.js
+++ contax_frontend/src/components/PlusButton.js
@@ -1,15 +1,20 @@
 import React from "react";
+import { useDispatch } from "react-redux";
 import { AiOutlinePlus } from "react-icons/ai";
+import { Link } from "react-router-dom";
+import { setCurrentContact } from "../state/ContactSlice";
+
 import "./scss/PlusButton.scss";
 
-import { Link, useRouteMatch } from "react-router-dom";
-
 const PlusButton = ({ history }) => {
-  const { url } = useRouteMatch();
-  console.log(url)
+  const dispatch = useDispatch();
   return (
-    <Link to='/app/add'>
-      <div className="w-auto position-fixed m-2 p-0 shadow" id="plus-button">
+    <Link to="/app/add">
+      <div
+        className="w-auto position-fixed m-2 p-0 shadow"
+        id="plus-button"
+        onClick={() => dispatch(setCurrentContact(null))}
+      >
         <AiOutlinePlus />
       </div>
     </Link>
