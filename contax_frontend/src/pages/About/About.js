8382132d4a52743c1f36bce53e2461f616117258
unknown <keegood8@gmail.com>
2021-08-16 14:14:18 -0700

    adjusted styles

--- contax_frontend/src/pages/About/About.js
+++ contax_frontend/src/pages/About/About.js
@@ -21,31 +21,37 @@ const About = () => {
     "React",
     "Redux/Thunk",
     "Axios",
+    "Reactstrap"
   ];
 
   return (
     <div className="rounded">
       <Row id="about-content" className="g-0">
-        <Col xs={12} md={{ size: 10, offset: 1 }} className="p-lg-3">
+        <Col xs={12}>
           <Jumbotron className="my-5 bg-secondary">
-            <Row className="g-0 bg-primary pb-5">
+            <Row className="g-0 bg-primary">
               <Col xs={12} className="">
                 <h1
                   id="about-title"
-                  className="display-1 text-center bg-secondary py-2 border-bottom border-3 border-info"
+                  className="
+                    display-1
+                    text-center
+                    bg-secondary
+                    py-2 m-0
+                    border-bottom border-3 border-info"
                 >
                   What is Contax?
                 </h1>
               </Col>
-
+            </Row>
+            <Row className="g-0 bg-primary pb-5">
               {/* WHAT IS CONTAX? */}
               <Col
-                xs={10}
+                xs={11}
                 md={8}
-                lg={6}
-                className="p-3 my-3 ms-1 ms-md-3 ms-lg-5 bg-light rounded shadow"
+                className="p-3 p-lg-5 my-4 my-lg-5 mx-auto ms-sm-3 ms-md-5 bg-light rounded shadow"
               >
-                Contax is a contact manager created by Keegan Good to represent
+                <span id="app-name">Contax</span> is a contact manager created by Keegan Good to represent
                 the culmination of one year of learning React.
                 <div className="mt-3">
                   <span id="dev-time-label" className="small">
@@ -57,15 +63,14 @@ const About = () => {
 
               {/* CHALLENGE LIST */}
               <Col
-                xs={10}
+                xs={11}
                 md={8}
-                lg={6}
-                className="mb-3 ms-auto me-1 me-md-3 me-lg-5 bg-light rounded shadow"
+                className="my-4 my-lg-5 mx-auto me-sm-3 me-md-5 bg-light rounded shadow"
               >
                 <h4 className="about-section-header display-4 bg-secondary p-3 rounded-top  border-bottom border-3 border-info">
                   Challenges
                 </h4>
-                <div className="about-section p-3 shadow">
+                <div className="about-section p-3 p-lg-5 shadow">
                   <List type="unstyled" id="challenges-list">
                     {CHALLENGE_LIST.map((text) => (
                       <AboutListItem
@@ -80,16 +85,15 @@ const About = () => {
 
               {/* TECH STACK LIST */}
               <Col
-                xs={10}
+                xs={11}
                 md={8}
-                lg={6}
-                className="mb-3 ms-1 ms-md-3 ms-lg-5 bg-light rounded"
+                className="my-4 mx-auto ms-md-3 ms-lg-5 bg-light rounded"
               >
                 <h4 className="about-section-header display-4 bg-secondary rounded-top p-3 border-bottom border-3 border-info">
                   <span>Tech Stack</span>
                   <IoExtensionPuzzleOutline className="mb-2 ms-2 p-1" />
                 </h4>
-                <div className="about-section p-3">
+                <div className="about-section p-3 p-lg-5">
                   <List type="unstyled" id="challenges-list">
                     {TECH_STACK_LIST.map((text) => (
                       <AboutListItem
