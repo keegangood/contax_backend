0ef32876f2eca5d067a2f451abd6b82557d599e4
unknown <keegood8@gmail.com>
2021-08-18 23:25:52 -0700

    added alerts component

--- contax_frontend/src/state/index.js
+++ contax_frontend/src/state/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from "@reduxjs/toolkit";
 import AuthSlice from "./AuthSlice";
 import ContactSlice from "./ContactSlice";
 import NoteSlice from "./NoteSlice";
+import AlertSlice from "./AlertSlice"
 
 const rootReducer = combineReducers({
   auth: AuthSlice,
   contacts: ContactSlice,
-  notes: NoteSlice
+  notes: NoteSlice,
+  alerts: AlertSlice
 });
 
 export default rootReducer;
