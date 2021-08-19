eedbf4b2177dd8036ae304eece40225096d08919
unknown <keegood8@gmail.com>
2021-08-19 06:34:47 -0700

    added alert when contact detail isn't found

--- contax_frontend/src/pages/Contacts/ContactForm.js
+++ contax_frontend/src/pages/Contacts/ContactForm.js
@@ -27,6 +27,7 @@ import "./scss/ContactForm.scss";
 import { addNote, setNewNoteText, setNotes } from "../../state/NoteSlice";
 import { getContactDetail, setCurrentContact } from "../../state/ContactSlice";
 import { requestAccessToken } from "../../state/AuthSlice";
+import { addAlert, removeAlert } from "../../state/AlertSlice";
 
 const PHONE_TYPES = ["CELL", "HOME", "WORK"];
 
@@ -44,18 +45,18 @@ const ContactForm = ({ formAction, onSubmit, history }) => {
     primaryPhone: "CELL",
     birthday: "",
   });
- 
+
   const { notes } = useSelector((state) => state.notes);
   const { currentContact, contactLoadingStatus } = useSelector(
     (state) => state.contacts
   );
-  const {contactId} = useParams();
+  const { contactId } = useParams();
 
-  useEffect(()=>{
-    if(formAction === 'add'){
+  useEffect(() => {
+    if (formAction === "add") {
       dispatch(setNotes([]));
     }
-  },[formAction])
+  }, [formAction]);
 
   // RETRIEVE SINGLE CONTACT
   // get the contact object for the contactId in url params
@@ -76,7 +77,11 @@ const ContactForm = ({ formAction, onSubmit, history }) => {
             dispatch(getContactDetail({ accessToken, contactId }))
               .then(unwrapResult)
               .then((res) => console.log("woo!", res))
-              .catch((err) => console.log("oops!", err));
+              .catch((err) => {
+                let alert = { text: err.message, alertType: "danger" };
+                dispatch(addAlert(alert));
+                history.push("/app");
+              });
           })
           .catch((err) => {
             console.log(err);
@@ -84,7 +89,7 @@ const ContactForm = ({ formAction, onSubmit, history }) => {
           });
       })();
     }
-  }, [contactId, currentContact, contactLoadingStatus]);
+  }, [contactId, currentContact]);
 
   useEffect(() => {
     if (currentContact) {
@@ -152,192 +157,196 @@ const ContactForm = ({ formAction, onSubmit, history }) => {
         </div>
       ) : (
         <>
-        <Form
-          className="rounded my-5 mx-2 mx-md-5"
-          id="contact-form"
-          onSubmit={(e) => {
-            e.preventDefault();
-            handleSubmit();
-          }}
-        >
-          <Label className="p-3 m-0" tag="h1" id="form-label">
-            <Row className="g-0">
-              {/* CONTACT AVATAR */}
-              <Col xs={2} md={1}>
-                <Avatar contact={currentContact} />
-              </Col>
-              {/* FORM ACTION */}
-              <Col xs={8} md={9} className="d-flex align-items-center ps-3 ps-sm-5 ps-lg-3">
-                <span>
-                  {formAction === "add" ? "Create Contact" : "Edit Contact"}
-                </span>
-              </Col>
-              {/* FORM CLOSE BUTTON */}
-              <Col
-                xs={2}
-                className="d-flex justify-content-end align-items-center"
-              >
-                <div>
-                  <AiOutlineCloseCircle
-                    className="close-form-icon mx-2"
-                    onClick={() => {
-                      dispatch(setCurrentContact(null));
-                      dispatch(setNotes([]));
-                      if (history.location.state) {
-                        history.push(history.location.state.referer);
-                      } else {
-                        history.push("/app");
-                      }
-                    }}
+          <Form
+            className="rounded my-5 mx-2 mx-md-5"
+            id="contact-form"
+            onSubmit={(e) => {
+              e.preventDefault();
+              handleSubmit();
+            }}
+          >
+            <Label className="p-3 m-0" tag="h1" id="form-label">
+              <Row className="g-0">
+                {/* CONTACT AVATAR */}
+                <Col xs={2} md={1}>
+                  <Avatar contact={currentContact} />
+                </Col>
+                {/* FORM ACTION */}
+                <Col
+                  xs={8}
+                  md={9}
+                  className="d-flex align-items-center ps-3 ps-sm-5 ps-lg-3"
+                >
+                  <span>
+                    {formAction === "add" ? "Create Contact" : "Edit Contact"}
+                  </span>
+                </Col>
+                {/* FORM CLOSE BUTTON */}
+                <Col
+                  xs={2}
+                  className="d-flex justify-content-end align-items-center"
+                >
+                  <div>
+                    <AiOutlineCloseCircle
+                      className="close-form-icon mx-2"
+                      onClick={() => {
+                        dispatch(setCurrentContact(null));
+                        dispatch(setNotes([]));
+                        if (history.location.state) {
+                          history.push(history.location.state.referer);
+                        } else {
+                          history.push("/app");
+                        }
+                      }}
+                    />
+                  </div>
+                </Col>
+              </Row>
+            </Label>
+
+            {/* NAME FIELDS */}
+            <FormGroup className="p-3 pt-4">
+              <Label htmlFor="name" className="field-label lead ps-1">
+                Name
+              </Label>
+              <Row className="g-0" id="name">
+                <Col xs={6} className="px-1">
+                  <Input
+                    type="text"
+                    name="firstName"
+                    placeholder="First"
+                    value={firstName}
+                    id="first-name"
+                    onChange={onChange}
                   />
-                </div>
-              </Col>
-            </Row>
-          </Label>
+                </Col>
+                <Col xs={6} className="px-1">
+                  <Input
+                    type="text"
+                    name="lastName"
+                    placeholder="Last"
+                    value={lastName}
+                    id="last-name"
+                    onChange={onChange}
+                  />
+                </Col>
+              </Row>
+            </FormGroup>
 
-          {/* NAME FIELDS */}
-          <FormGroup className="p-3 pt-4">
-            <Label htmlFor="name" className="field-label lead ps-1">
-              Name
-            </Label>
-            <Row className="g-0" id="name">
-              <Col xs={6} className="px-1">
-                <Input
-                  type="text"
-                  name="firstName"
-                  placeholder="First"
-                  value={firstName}
-                  id="first-name"
-                  onChange={onChange}
-                />
-              </Col>
-              <Col xs={6} className="px-1">
-                <Input
-                  type="text"
-                  name="lastName"
-                  placeholder="Last"
-                  value={lastName}
-                  id="last-name"
-                  onChange={onChange}
-                />
-              </Col>
-            </Row>
-          </FormGroup>
+            {/* EMAIL FIELD */}
+            <FormGroup className="p-3 pb-4 mb-2">
+              <Label htmlFor="email" className="field-label lead ps-1">
+                Email
+              </Label>
+              <Input
+                id="email"
+                type="email"
+                placeholder="user@example.com"
+                name="email"
+                value={email}
+                onChange={onChange}
+              />
+            </FormGroup>
 
-          {/* EMAIL FIELD */}
-          <FormGroup className="p-3 pb-4 mb-2">
-            <Label htmlFor="email" className="field-label lead ps-1">
-              Email
-            </Label>
-            <Input
-              id="email"
-              type="email"
-              placeholder="user@example.com"
-              name="email"
-              value={email}
-              onChange={onChange}
-            />
-          </FormGroup>
+            {/* PHONE NUMBER FIELDS */}
+            <Row className="g-0 mb-2 h-100">
+              <Col xs={12} md={6}>
+                <FormGroup className="p-3 pt-0 pe-lg-0 border-end border-secondary">
+                  <Row className="g-0">
+                    <Col xs={9}>
+                      <Label htmlFor="phone" className="field-label py-2 lead">
+                        Phone{" "}
+                        <AiOutlineInfoCircle
+                          id="phoneInfo"
+                          onMouseEnter={togglePhoneTooltip}
+                          onMouseLeave={togglePhoneTooltip}
+                        ></AiOutlineInfoCircle>
+                        <Tooltip
+                          placement={"right"}
+                          target="phoneInfo"
+                          isOpen={phoneTooltipOpen}
+                          toggle={togglePhoneTooltip}
+                        >
+                          10-digit phone numbers only
+                        </Tooltip>
+                      </Label>
+                    </Col>
+                    <Col xs={3} className="field-label pt-4 text-center">
+                      Primary
+                    </Col>
+                  </Row>
 
-          {/* PHONE NUMBER FIELDS */}
-          <Row className="g-0 mb-2 h-100">
-            <Col xs={12} md={6}>
-              <FormGroup className="p-3 pt-0 pe-lg-0 border-end border-secondary">
-                <Row className="g-0">
-                  <Col xs={9}>
-                    <Label htmlFor="phone" className="field-label py-2 lead">
-                      Phone{" "}
-                      <AiOutlineInfoCircle
-                        id="phoneInfo"
-                        onMouseEnter={togglePhoneTooltip}
-                        onMouseLeave={togglePhoneTooltip}
-                      ></AiOutlineInfoCircle>
-                      <Tooltip
-                        placement={"right"}
-                        target="phoneInfo"
-                        isOpen={phoneTooltipOpen}
-                        toggle={togglePhoneTooltip}
-                      >
-                        10-digit phone numbers only
-                      </Tooltip>
-                    </Label>
-                  </Col>
-                  <Col xs={3} className="field-label pt-4 text-center">
-                    Primary
-                  </Col>
-                </Row>
-
-                <Row className="g-0" id="phone">
-                  {PHONE_TYPES.map((phoneType, i) => (
-                    <Fragment key={i}>
-                      <Col
-                        xs={2}
-                        className="mb-2 field-label ps-1 d-flex flex-column justify-content-center"
-                      >
-                        {titleize(phoneType)}
-                      </Col>
-                      <Col xs={7} className="mb-2 ps-3">
-                        <Input
-                          type="tel"
-                          name={`${phoneType.toLowerCase()}PhoneNumber`}
-                          value={
-                            formData[`${phoneType.toLowerCase()}PhoneNumber`]
-                          }
-                          onChange={onChange}
-                        />
-                      </Col>
-                      <Col
-                        xs={3}
-                        className="
+                  <Row className="g-0" id="phone">
+                    {PHONE_TYPES.map((phoneType, i) => (
+                      <Fragment key={i}>
+                        <Col
+                          xs={2}
+                          className="mb-2 field-label ps-1 d-flex flex-column justify-content-center"
+                        >
+                          {titleize(phoneType)}
+                        </Col>
+                        <Col xs={7} className="mb-2 ps-3">
+                          <Input
+                            type="tel"
+                            name={`${phoneType.toLowerCase()}PhoneNumber`}
+                            value={
+                              formData[`${phoneType.toLowerCase()}PhoneNumber`]
+                            }
+                            onChange={onChange}
+                          />
+                        </Col>
+                        <Col
+                          xs={3}
+                          className="
                     mb-2 
                     d-flex flex-column 
                     justify-content-center 
                     align-items-center"
-                      >
-                        <Input
-                          type="radio"
-                          checked={primaryPhone.toUpperCase() === phoneType}
-                          id={`primaryPhoneRadio${i}`}
-                          name={`primaryPhoneRadio${i}`}
-                          onChange={changePrimaryPhone}
-                          data-phone-type={phoneType}
-                        />
-                      </Col>
-                    </Fragment>
-                  ))}
-                </Row>
-              </FormGroup>
-            </Col>
-            <Col xs={12} md={6} id="birthday-field">
-              <FormGroup className="p-3 pt-0">
-                <Label htmlFor="birthday" className="field-label py-2 lead">
-                  Birthday
-                </Label>
-                <Input
-                  className="form-field text-secondary"
-                  id="birthday"
-                  type="date"
-                  name="birthday"
-                  value={birthday}
-                  onChange={onChange}
-                />
-              </FormGroup>
-            </Col>
-          </Row>
-
-          {/* NOTES FIELD */}
-          <Notes />
-
-          <FormGroup>
-            <Row className="g-0">
-              <Col xs={{ size: 6, offset: 3 }} className="text-center p-3">
-                <Button color="info" size="lg" type="submit">
-                  Save
-                </Button>
+                        >
+                          <Input
+                            type="radio"
+                            checked={primaryPhone.toUpperCase() === phoneType}
+                            id={`primaryPhoneRadio${i}`}
+                            name={`primaryPhoneRadio${i}`}
+                            onChange={changePrimaryPhone}
+                            data-phone-type={phoneType}
+                          />
+                        </Col>
+                      </Fragment>
+                    ))}
+                  </Row>
+                </FormGroup>
+              </Col>
+              <Col xs={12} md={6} id="birthday-field">
+                <FormGroup className="p-3 pt-0">
+                  <Label htmlFor="birthday" className="field-label py-2 lead">
+                    Birthday
+                  </Label>
+                  <Input
+                    className="form-field text-secondary"
+                    id="birthday"
+                    type="date"
+                    name="birthday"
+                    value={birthday}
+                    onChange={onChange}
+                  />
+                </FormGroup>
               </Col>
             </Row>
-          </FormGroup>
-        </Form>
+
+            {/* NOTES FIELD */}
+            <Notes />
+
+            <FormGroup>
+              <Row className="g-0">
+                <Col xs={{ size: 6, offset: 3 }} className="text-center p-3">
+                  <Button color="info" size="lg" type="submit">
+                    Save
+                  </Button>
+                </Col>
+              </Row>
+            </FormGroup>
+          </Form>
         </>
       )}
     </>
