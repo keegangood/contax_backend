9a23c524041db9ec2a90883cc2b836934d196cf2
unknown <keegood8@gmail.com>
2021-08-25 10:42:40 -0700

    adjusted cookie domain

--- users/views.py
+++ users/views.py
@@ -81,9 +81,9 @@ def register(request):
             key='refreshtoken',
             value=refresh_token,
             httponly=True,  # to help prevent XSS
-            samesite='strict',  # to help prevent XSS
-            domain='localhost',  # change in production
-            # secure=True # for https connections only
+            samesite='Lax',  # to help prevent XSS
+            domain='netlify.app',  # change in production
+            secure=True # for https connections only
         )
 
         # return successful response
@@ -155,7 +155,7 @@ def login(request):
         key='refreshtoken',  # cookie name
         value=refresh_token,  # cookie value
         httponly=True,  # to help prevent XSS
-        domain='.netlify.app',  # change in production
+        domain='netlify.app',  # change in production
         samesite='Lax',  # to help prevent XSS
         # secure=True # for https connections only
     )
@@ -320,7 +320,7 @@ def extend_token(request):
         value=new_refresh_token,  # cookie value
         httponly=True,  # to help prevent XSS attacks
         samesite='Lax',  # to help prevent XSS attacks
-        domain='.netlify.app',  # change in production
+        domain='netlify.app',  # change in production
         secure=True # for https connections only
     )
 
