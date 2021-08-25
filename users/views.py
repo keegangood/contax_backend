990d71921242f0f9b294336a917e661a90cd5e8c
unknown <keegood8@gmail.com>
2021-08-25 11:43:24 -0700

    cors nonsense

--- users/views.py
+++ users/views.py
@@ -320,7 +320,7 @@ def extend_token(request):
         value=new_refresh_token,  # cookie value
         httponly=True,  # to help prevent XSS attacks
         samesite='None',  # to help prevent XSS attacks
-        domain='https://wonderful-lamport-b2455b.netlify.app',  # change in production
+        # domain='https://wonderful-lamport-b2455b.netlify.app',  # change in production
         secure=True # for https connections only
     )
 
