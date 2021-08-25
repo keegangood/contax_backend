a28a00af66b8c7d02ba640b6023b6d466a984421
unknown <keegood8@gmail.com>
2021-08-25 11:44:59 -0700

    cors nonsense

--- users/views.py
+++ users/views.py
@@ -155,8 +155,8 @@ def login(request):
         key='refreshtoken',  # cookie name
         value=refresh_token,  # cookie value
         httponly=True,  # to help prevent XSS
-        domain='netlify.app',  # change in production
-        samesite='Lax',  # to help prevent XSS
+        # domain='netlify.app',  # change in production
+        samesite='None',  # to help prevent XSS
         secure=True # for https connections only
     )
 
