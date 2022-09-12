0c334300ee61f84f07347f4433d15b7996b0291e
KG <keegood8@gmail.com>
2022-09-12 09:29:51 -0700

    updated requirements.txt

--- contacts/models.py
+++ contacts/models.py
@@ -19,7 +19,7 @@ class Contact(models.Model):
     birthday = models.DateField("birthday", blank=True, null=True)
 
     phone_regex = RegexValidator(
-        regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
+        regex=r'\d{10}$', message="Phone number must be entered in the format: '999999999'. Up to 10 digits allowed.")
     home_phone_number = models.CharField(
         validators=[phone_regex], max_length=17, blank=True, null=True)
     cell_phone_number = models.CharField(
